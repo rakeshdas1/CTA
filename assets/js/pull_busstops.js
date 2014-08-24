0e9a9ae6e76e7230b1615e25f9f67e54d4f17e5c
rakeshdas1 <rakeshdaskowshik@gmail.com>
2014-08-24 18:54:58 -0500

    added bustop pull

--- assets/js/pull_busstops.js
+++ assets/js/pull_busstops.js
@@ -1,1 +1,32 @@
-$.getJSON('http://data.cityofchicago.org/resource/jyb9-n7fm.json', function)
+$.getJSON('http://data.cityofchicago.org/resource/jyb9-n7fm.json', function(bstops){
+	$('#bstoplist').empty();
+
+	$.each(bstops, function(i, bstop){
+		$('#bstoplist').append(generateBStopLink(bstop));
+
+	}),
+	function (data){
+		console.log(data)
+	};
+	$('#bstoplist').listview('refresh');
+
+});
+
+function generateLStopLink(Bstop){
+	return '<li><a href="javascript:void(0)'
+			+'"onclick="gotoStopDetailPage(\''
+			+bstop.route
+			+'\',\''
+			+bstop.rides+'\')">'
+			+bstop.route
+			+'</a></li>';
+}
+function goToStopDetailPage (stopName){
+
+  var stopPage = $("<div data-role='page' data-url=station><div data-role='header'><h1>"
+                  + stopName + "</h1></div><div data-role='content'></div><div data-role='footer'><h4>" 
+                  + stopName + "</h4></div></div>");	
+  stopPage.appendTo( $.mobile.pageContainer );
+
+  $.mobile.changePage( stopPage );
+}
